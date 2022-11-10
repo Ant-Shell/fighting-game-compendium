26c949ac3e207c855ba5712d17889cb4f331b21a
Anthony Shellman <atshellman@gmail.com>
2022-11-10 10:07:02 -0700

    Update App with test data and tentative components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,22 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const [fightingGames, setFightingGames] = useState([])
 
+
+  useEffect = (() => {
+    setFightingGames([{name: "Street Fighter 5", background_image: "https://media.rawg.io/media/games/ce2/ce2463db40cec363f360c29ddcc56884.jpg", id: 742771}])
+  }, [])
+
+  return(
+    <main>
+      {/* <Header />  */}
+      {/* GameContainer /> */}
+      {/* SingleGame */}
+      {/* Footer /> */}
+      {/* ErrorPage /> */}
+    </main>
+  )
 }
 
 export default App;
