84dc9cb30427e58fc267bda46760af57b2ab1f64
Anthony Shellman <atshellman@gmail.com>
2022-11-10 10:46:46 -0700

    Pass props and render card

---
+++ src/components/GamesContainer/GamesContainer.js
@@ -0,0 +1,24 @@
+import GameCard from "../GameCard/GameCard";
+import "./GamesContainer.css"
+
+const GamesContainer = ({ fightingGames }) => {
+  const fightingGamesList = fightingGames.map(game => {
+    const { name, background_image, id } = game
+    return (
+      <GameCard
+        id={id}
+        name={name}
+        background_image={background_image}
+        key={id}
+      />
+    )
+  })
+
+  return(
+    <div>
+      {fightingGamesList}
+    </div>
+  )
+}
+
+export default GamesContainer
