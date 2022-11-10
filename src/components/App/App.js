a5b938a84b1b05c6fa31a57420174ffe9d42c872
Anthony Shellman <atshellman@gmail.com>
2022-11-10 10:44:00 -0700

    Add test data to state and pass props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react';
+import GamesContainer from '../GamesContainer/GamesContainer';
 import './App.css';
 
 const App = () => {
   const [fightingGames, setFightingGames] = useState([])
 
 
-  useEffect = (() => {
-    setFightingGames([{name: "Street Fighter 5", background_image: "https://media.rawg.io/media/games/ce2/ce2463db40cec363f360c29ddcc56884.jpg", id: 742771}])
+  useEffect(() => {
+    setFightingGames([{name: "Street Fighter 6", background_image: "https://media.rawg.io/media/games/ce2/ce2463db40cec363f360c29ddcc56884.jpg", id: 742771}])
   }, [])
 
   return(
     <main>
       {/* <Header />  */}
-      {/* GameContainer /> */}
+      <GamesContainer fightingGames={fightingGames}/>
       {/* SingleGame */}
       {/* Footer /> */}
       {/* ErrorPage /> */}
