442cc18c7c5f9cc0c3fb4fd7c9cbb72cb188ca17
Anthony Shellman <atshellman@gmail.com>
2022-11-10 20:49:21 -0700

    Add rendering for data passed as props

--- src/components/SingleGameCard/SingleGameCard.js
+++ src/components/SingleGameCard/SingleGameCard.js
@@ -1,16 +1,16 @@
 import "./SingleGameCard.js"
 
-const SingleGameCard = ( {name, description, metacritic, released, esrb_rating, website} ) => {
+const SingleGameCard = ( {name, description, metacritic, released, esrb_rating, developers, publishers, platforms, website} ) => {
   return (
   <div className="single-card" >
   <h2>{name}</h2>
   <p>Description: {description}</p>
   <p>MetaCritic Rating: {metacritic}</p>
   <p>Release Date: {released}</p>
-  {/* <p>Platforms: {platforms}</p> */}
-  {/* <p>Developers: {}</p> */}
-  {/* <p>Publishers: {}</p> */}
-  {/* <p>ESRB Rating: {esrb_rating}</p> */}
+  <p>Platforms: {platforms}</p>
+  <p>Developers: {developers}</p>
+  <p>Publishers: {publishers}</p>
+  <p>ESRB Rating: {esrb_rating}</p>
   <p>Website: {website}</p>
 </div>
 )
