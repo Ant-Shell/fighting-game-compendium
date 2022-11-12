db3429bbe07cee05b04523f6d3fe08fbda69d3e6
Anthony Shellman <atshellman@gmail.com>
2022-11-12 14:09:31 -0700

    Create funtion for searching

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,21 @@
 import { useState } from "react"
 import "./SearchBar.css"
 
-const SearchBar = () => {
+const SearchBar = ({ searchForGame }) => {
   const [userInput, setUserInput] = useState('')
 
   // function that calls setUserInput and also
   // performs a search based on user input
-  // put search result in separate state in app?
+  // put search result in separate state in app (array)?
   // render that if doing a search, else ender all
-  // games?
+  // games - based on boolean?
+  // ex: if userInput has a value, flip
+  // boolean to true and render the search results array
+
+  const searchAction = (event) => {
+    setUserInput(event)
+    searchForGame(event)
+  }
 
   return (
     <form>
@@ -17,7 +24,7 @@ const SearchBar = () => {
       placeholder="Search Games"
       name="search"
       value={userInput}
-      onChange={(event) => setUserInput(event.target.value)}
+      onChange={(event) => searchAction(event.target.value)}
       />
     </form>
   )
