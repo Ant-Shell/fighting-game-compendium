019ac2ec90b29de5931d58b7a4b8a788f6c068fb
Anthony Shellman <atshellman@gmail.com>
2022-11-12 09:20:58 -0700

    Add initial header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className='banner'>The Fighting Game Compendium</h1>
+    </header>
+  )
+}
+
+
+export default Header
