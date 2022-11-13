809ff6963fbcccdc2fb1d244cf78b57a39c57ee0
Anthony Shellman <atshellman@gmail.com>
2022-11-12 18:54:03 -0700

    Remove comments

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,14 +4,6 @@ import "./SearchBar.css"
 const SearchBar = ({ searchForGame, foundSearchResults }) => {
   const [userInput, setUserInput] = useState('')
 
-  // function that calls setUserInput and also
-  // performs a search based on user input
-  // put search result in separate state in app (array)?
-  // render that if doing a search, else ender all
-  // games - based on boolean?
-  // ex: if userInput has a value, flip
-  // boolean to true and render the search results array
-
   const searchAction = (event) => {
     setUserInput(event)
     searchForGame(event)
